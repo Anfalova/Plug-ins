203aa0023f00d355894bd30741462715560601e3
Евгений Анфалов <anfalovd10@yandex.ru>
2016-11-05 23:28:48 +0500

    fix

---
+++ Application/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework;
+using System.Reflection;
+using System.IO;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            string directory = "C:\\Users\\all\\Documents\\Visual Studio 2015\\Projects\\Plug-ins\\Plugins\\";
+            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+            foreach (FileInfo file in directoryInfo.GetFiles("*.dll"))
+            {
+                Assembly assembly = Assembly.LoadFrom(file.FullName);
+                foreach (var type in assembly.GetTypes())
+                    if (type.GetInterface("IPlugin") != null)
+                        Console.WriteLine((Activator.CreateInstance(type) as IPlugin).Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Framework/IPlugin.cs
+++ Framework/IPlugin.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Framework
 {
-    public class Class1
+    public interface IPlugin
     {
+        String Name { get; }
     }
 }
